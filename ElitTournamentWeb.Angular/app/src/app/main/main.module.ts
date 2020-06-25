5549f120b5be8aec46d7b3004d93722432f62e40
vlad <vladislavvillya@gmail.com>
2020-06-25 16:31:05 +0300

    create main page

--- ElitTournamentWeb.Angular/app/src/app/main/main.module.ts
+++ ElitTournamentWeb.Angular/app/src/app/main/main.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MainHomeComponent } from './main-home/main-home.component';
 import {SharedModule} from '../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
+import {MatGridListModule} from '@angular/material';
 
 @NgModule({
   declarations: [MainHomeComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    MainRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        MainRoutingModule,
+        MatGridListModule
+    ]
 })
 export class MainModule { }
